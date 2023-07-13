b88339cc358a94e035d87b5aa4a49298eb2edeec
seohee0925 <appleid21@sookmyung.ac.kr>
2023-07-13 21:06:59 +0900

    Feat: 여행지 입력 저장

--- MatchZzic/main/models.py
+++ MatchZzic/main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django import forms
 
 # Create your models here.
+class Post (models.Model):
+    place = models.TextField('동행할 여행지', max_length=30, default='')
+    
+    def __str__(self):
+        return self.place
--- MatchZzic/main/views.py
+++ MatchZzic/main/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import Post
+from account.models import User
+from .forms import PostForm
 
-# Create your views here.
 def main(request):
-    return render(request, 'main.html')
+    # 해당 user에 여행지 저장
+    # user_id = request.session.get('user')
+    # user = User.objects.get(userId=user_id)
+    
+    # 여행지 저장
+    if request.method == 'POST':
+        place = PostForm(request.POST)
+        if place.is_valid():
+            data = place.save(commit=False)
+            #data.user = user
+            data.place = request.POST.get('placeName')
+            data.save()
+            return redirect('main')
+    else:
+        place = PostForm()
+    return render(request, 'main.html', {'place':place})
+
+
+def match(request):
+    return render(request, 'match.html')
