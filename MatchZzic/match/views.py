f63ee67afa66ae3fe448d67712df444e38970a35
chaehyeon02 <rhee2015@gmail.com>
2023-07-15 01:59:36 +0900

    match/finish backend

--- MatchZzic/match/views.py
+++ MatchZzic/match/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from account.models import User
 
-def match2(request):
-    # user_id = request.session.get('user')
-    user_id = 'kim12'
+def match(request):
+    user_id = request.session['user']
+    # user_id = 'kim12'
     user = User.objects.get(userId=user_id)
 
     # 여행지가 같은 사람 중 랜덤으로 뽑기
@@ -13,90 +13,54 @@ def match2(request):
         travel_to = user.travel_to
     ).order_by('?')[0]
 
-    match_user2_info = User.objects.filter(
+    match_user_info = User.objects.filter(
         userName = random_user
     ).values('userName', 'userType1', 'userType2', 'userType3', 'userType4')
-    match_user2_name = [user['userName'] for user in match_user2_info]
-    match_user2_type1 = [user['userType1'] for user in match_user2_info]
-    match_user2_type2 = [user['userType2'] for user in match_user2_info]
-    match_user2_type3 = [user['userType3'] for user in match_user2_info]
-    match_user2_type4 = [user['userType4'] for user in match_user2_info]
-    request.session['match_user2'] = match_user2_name[0]
+    match_user_name = [user['userName'] for user in match_user_info]
+    match_user_type1 = [user['userType1'] for user in match_user_info]
+    match_user_type2 = [user['userType2'] for user in match_user_info]
+    match_user_type3 = [user['userType3'] for user in match_user_info]
+    match_user_type4 = [user['userType4'] for user in match_user_info]
+    request.session['match_user'] = match_user_name[0]
+
+    # if match_user_type1[0] == 'option1':
+    #     ans1 = '계획형'
+    # else:
+    #     ans1 = '즉흥형'
+    
+    # if match_user_type2[0] == 'option3':
+    #     ans2 = '바쁘게 움직이는 여행'
+    # else:
+    #     ans2 = '오직 휴식을 위한 여행'
+
+    # if match_user_type3[0] == 'option1':
+    #     ans3 = '사진 찍는 거 좋아요'
+    # else:
+    #     ans3 = '사진 찍는 거 싫어요'
+
+    # if match_user_type4[0] == 'option1':
+    #     ans4 = '번화가'
+    # else:
+    #     ans4 = '자연, 시골'
 
     return render(request, 'match_test.html', 
                 {
                 'user': user, #temp
-                'match_user2': match_user2_name[0],
-                'match_user2_type1': match_user2_type1,
-                'match_user2_type2': match_user2_type2,
-                'match_user2_type3': match_user2_type3,
-                'match_user2_type4': match_user2_type4, })
-
-
-def match(request):
-    # user_id = request.session.get('user')
-    user_id = 'kim12'
-    user = User.objects.get(userId=user_id)
-
-    # 현재 로그인한 사용자와 답변이 일치하는 사용자 찾기
-    try: 
-        match_user = User.objects.exclude(
-            userId=user_id
-        ).filter(
-            userType1=user.userType1,
-            userType2=user.userType2,
-            userType3=user.userType3,
-            userType4=user.userType4, 
-        ).values('userName', 'userType1', 'userType2', 'userType3', 'userType4')
-    except User.DoesNotExist:
-        match_user = None
-        
-    match_user_name = [user['userName'] for user in match_user]
-    match_user_type1 = [user['userType1'] for user in match_user]
-    match_user_type2 = [user['userType2'] for user in match_user]
-    match_user_type3 = [user['userType3'] for user in match_user]
-    match_user_type4 = [user['userType4'] for user in match_user]
-
-    if match_user_type1[0] == 'option1':
-        ans1 = '계획형'
-    else:
-        ans1 = '즉흥형'
-    
-    if match_user_type2[0] == 'option3':
-        ans2 = '바쁘게 움직이는 여행'
-    else:
-        ans2 = '오직 휴식을 위한 여행'
-
-    if match_user_type3[0] == 'option1':
-        ans3 = '사진 찍는 거 좋아요'
-    else:
-        ans3 = '사진 찍는 거 싫어요'
-
-    if match_user_type4[0] == 'option1':
-        ans4 = '번화가'
-    else:
-        ans4 = '자연, 시골'
-
-    request.session['match_user_name'] = match_user_name[0]
-    return render(request, 'match_test.html', 
-                {
-                    'match_user_name': match_user_name[0], 
-                'ans1': ans1, 
-                'ans2': ans2, 
-                'ans3': ans3, 
-                'ans4': ans4, })
-
-
+                'match_user2': match_user_name[0],
+                'match_user2_type1': match_user_type1,
+                'match_user2_type2': match_user_type2,
+                'match_user2_type3': match_user_type3,
+                'match_user2_type4': match_user_type4, })
 
 # match_finish
 def finish(request):
     # 임시로 user 설정해둠. 나중에 지우기!!
-    request.session['user'] = 'kim'
+    # request.session['user'] = 'kim'
     user = request.session['user']
-    match_user = request.session['match_user2']
+    match_user = request.session['match_user']
 
     user_info = User.objects.filter(
-        userName = user
+        userId = user
     ).values('travel_to')
     
     user_travel_to = [user['travel_to'] for user in user_info]
@@ -133,7 +97,7 @@ def finish(request):
 
     contents = {
         'user_name': user,
-        'user_travel_to': user_travel_to[0],
+        'user_travel_to': user_travel_to,
         'match_user_name': match_user,
         'ans1': ans1,
         'ans2': ans2,
