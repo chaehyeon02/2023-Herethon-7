0a5da1611f6be6346b606e2819562bda089338a6
chaehyeon02 <rhee2015@gmail.com>
2023-07-14 21:24:46 +0900

    match 에러 처리

--- MatchZzic/match/views.py
+++ MatchZzic/match/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from .models import MatchUser
 import json
 
 from account.models import User
@@ -18,14 +17,27 @@ def match(request):
     ]
 
     # match_user에 userType1, userType2, userType3, userType4가 현재 로그인한 사용자와 일치하는 행 찾기
-    match_user = User.objects.exclude(
-        userId=user_id
-    ).filter(
-        userType1__in=user_types,
-        userType2__in=user_types,
-        userType3__in=user_types,
-        userType4__in=user_types,
-    ).values('userName', 'userType1', 'userType2', 'userType3', 'userType4')
+    try: 
+        match_user = User.objects.exclude(
+            userId=user_id
+        ).filter(
+            userType1__in=user_types,
+            userType2__in=user_types,
+            userType3__in=user_types,
+            userType4__in=user_types, 
+        ).values('userName', 'userType1', 'userType2', 'userType3', 'userType4')
+    except User.DoesNotExist:
+        match_user = None
+
+#     In [4]: def get_random2():
+#    ...:     max_id = Category.objects.all().aggregate(max_id=Max("id"))['max_id']
+#    ...:     pk = random.randint(1, max_id)
+#    ...:     return Category.objects.get(pk=pk)
+
+    # max_id = User.objects.all().aggregate(max_id = Max("user"))
+    # match_user = User.object.filter(
+
+    # )
         
     match_user_name = [user['userName'] for user in match_user]
     match_user_type1 = [user['userType1'] for user in match_user]
