5373c41c8c79b317a0461703baa9beab2e90897d
chaehyeon02 <rhee2015@gmail.com>
2023-07-14 22:01:40 +0900

    match_finish complete

--- MatchZzic/match/views.py
+++ MatchZzic/match/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-import json
-
 from account.models import User
+from django.db.models import Max
+import random
 
 def match(request):
     # user_id = request.session.get('user')
@@ -34,7 +34,12 @@ def match(request):
 #    ...:     pk = random.randint(1, max_id)
 #    ...:     return Category.objects.get(pk=pk)
 
-    # max_id = User.objects.all().aggregate(max_id = Max("user"))
+    # count = MyModel.objects.aggregate(count=Count('id'))['count']
+    # random_index = randint(0, count - 1)
+    # first = MyModel.objects.all()[random_index]
+
+    # count = User.objects.all().aggregate(max_id = Max("userId"))['count']
+    # random_index = randint(0, count - 1)
     # match_user = User.object.filter(
 
     # )
@@ -66,7 +71,7 @@ def match(request):
         ans4 = '자연, 시골'
 
     request.session['match_user_name'] = match_user_name[0]
-    return render(request, 'match.html', 
+    return render(request, 'match_test.html', 
                 {'match_user_name': match_user_name[0], 'ans1': ans1, 'ans2': ans2, 'ans3': ans3, 'ans4': ans4 })
 
 # match_finish
@@ -76,15 +81,50 @@ def finish(request):
     user = request.session['user']
     match_user = request.session['match_user_name']
 
-    match_user_travel = User.objects.filter(
+    user_info = User.objects.filter(
         userName = user
     ).values('travel_to')
-
     
+    user_travel_to = [user['travel_to'] for user in user_info]
+
+    # 매칭된 유저의 정보 받아오기
+    match_user_info = User.objects.filter(
+        userName = match_user
+    ).values('userType1', 'userType2', 'userType3', 'userType4')
+    
+    match_user_type1 = [user['userType1'] for user in match_user_info]
+    match_user_type2 = [user['userType2'] for user in match_user_info]
+    match_user_type3 = [user['userType3'] for user in match_user_info]
+    match_user_type4 = [user['userType4'] for user in match_user_info]
+
+    if match_user_type1[0] == 'option1':
+        ans1 = '계획형'
+    else:
+        ans1 = '즉흥형'
+    
+    if match_user_type2[0] == 'option3':
+        ans2 = '바쁘게 움직이는 여행'
+    else:
+        ans2 = '오직 휴식을 위한 여행'
+
+    if match_user_type3[0] == 'option1':
+        ans3 = '사진 찍는 거 좋아요'
+    else:
+        ans3 = '사진 찍는 거 싫어요'
+
+    if match_user_type4[0] == 'option1':
+        ans4 = '번화가'
+    else:
+        ans4 = '자연, 시골'
 
     contents = {
         'user_name': user,
-        'match_user_travel': match_user_travel,
+        'user_travel_to': user_travel_to[0],
+        'match_user_name': match_user,
+        'ans1': ans1,
+        'ans2': ans2,
+        'ans3': ans3,
+        'ans4': ans4,
     }
 
     return render(request, 'match_finish_test.html', contents)
